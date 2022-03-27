4d24fae0331bfba8ef65772069a2b71b6af25a3b
Adam Zięba <adam.maciej.zieba@gmail.com>
2022-03-27 23:28:26 +0200

    some changes

--- nodetest.js
+++ nodetest.js
@@ -1,6 +1,10 @@
 var http = require('http');
-
+var counter = 0;
 http.createServer(function (req, res) {
+    if(req.url === '/favicon.ico'){
+        console.log('favicon');
+        return;     }
+    counter = counter + 1;
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end('Hello World!     '+counter);
 }).listen(8080);
