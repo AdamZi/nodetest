fda884905cd0b00224e32ce16c96d484f621daf2
Adam Zięba <adam.maciej.zieba@gmail.com>
2022-03-28 09:37:58 +0200

    refactor

--- nodetest.js
+++ nodetest.js
@@ -1,16 +1,31 @@
-var http = require('http');
+var http = require("http");
+const { exec } = require("child_process");
+
 let counter = 0;
-http.createServer(function (req, res) {
-    if(req.url === '/favicon.ico'){
-       // console.log('favicon');
-        return;     }
+let time = 0;
+
+http
+  .createServer(function (req, res) {
+    if (req.url === "/favicon.ico") {
+      // console.log('favicon');
+      return;
+    }
     counter = counter + 1;
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!     '+counter);
-}).listen(8080);
+    exec("git branch", (error, stdout, stderr) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("Hello World!     " + counter + "  time:" + time + "  " + stdout);
+    });
+    //dhdfjpefpfjk
+  })
+  .listen(8080);
+
 console.log("Server running, press ctrl-c to exit");
 setTimeout(function () {
-    console.log("2 minutes passed");
-    console.log("Server stopped");
-    process.exit();
-    },120000);
+  console.log("2 minutes passed");
+  console.log("Server stopped");
+  process.exit();
+}, 120000);
+
+setInterval(() => {
+  time += 1;
+}, 1000);
