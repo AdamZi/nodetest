4f6f7f290c246384bcdd5e26ec77a34c6690f6b8
Adam Zięba <adam.maciej.zieba@gmail.com>
2022-04-02 01:23:35 +0200

    some changes

--- nodetest.js
+++ nodetest.js
@@ -3,14 +3,30 @@ const file = require("fs");
 const fileName1 = "form.html";
 const fileName2 = "dogs.json";
 
+//var url1 = require("url");
 var dogs = [];
 
 http
   .createServer(function (req, res) {
-    file.readFile("form.html", function (err, data) {
+    var url1 = require("url");
+    var q = url1.parse(req.url, true);
+    if (req.url === "/favicon.ico") {
+      // console.log('favicon');
+      return;
+    }
+    file.readFile(fileName1, function (err, data) {
+      //var q = "dfdfd";
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
-      res.end();
+      res.write(req.url);
+      if ("name" in q.query || "birth" in q.query) {
+        console.log(q.query);
+        //  res.end();
+      }
+      file.readFile(fileName2, function (err, data) {
+        res.write(data);
+        res.end();
+      });
     });
   })
   .listen(8080);
