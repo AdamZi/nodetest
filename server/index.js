3f598bcb50553b0046c4134e30037c90236e658c
Mateusz Rorat <mateuszrorat@gmail.com>
2022-05-09 22:32:00 +0200

    main file changed to index to fit more universal patterns parts of nodetest.js moved to separate files

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const publicDirName = 'public';
+const api = require('./routes');
+const myPort = process.env.PORT || 8080;
+
+app.use(express.static(path.resolve(__dirname, '../', publicDirName)));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api', api);
+
+app.listen(myPort);
