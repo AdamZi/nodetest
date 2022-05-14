8078c23375169062c3b69fb2ffbfcb91cf583b21
Adam Zięba <adam.maciej.zieba@gmail.com>
2022-05-14 21:17:37 +0200

    function readDogs added

--- server/db/index.js
+++ server/db/index.js
@@ -1,30 +1,40 @@
-const file = require('fs');
-const path = require('path');
-const fileData = path.resolve('db', 'data', 'dogs.json');
-const Dog = require('../models/Dog');
+const file = require("fs");
+const path = require("path");
+const fileData = path.resolve("db", "data", "dogs.json");
+const Dog = require("../models/Dog");
 
-const getDogs = () => {
+const dogs = [];
+
+function readDogs() {
   try {
-    return JSON.parse(file.readFileSync(fileData)).map((dog) => {
-      return new Dog(dog.name, dog.birth);
+    JSON.parse(file.readFileSync(fileData, "utf8")).forEach(dog => {
+      dogs.push(new Dog(dog.name, dog.birth));
     });
   } catch (err) {
-    console.log(`Error reading file from disk: ${err} ${fileData}`);
+    console.log(`Error reading file from disk: ${err}`);
   }
+}
+
+const getDogs = () => {
+  // try {
+  //   return JSON.parse(file.readFileSync(fileData)).map(dog => {
+  //     return new Dog(dog.name, dog.birth);
+  //   });
+  // } catch (err) {
+  //   console.log(`Error reading file from disk: ${err} ${fileData}`);
+  // }
+  return dogs;
 };
 
-const saveDog = (dog) => {
-  const dogs = getDogs();
+const saveDog = dog => {
+  // const dogs = getDogs();
   dogs.unshift(dog);
-  file.writeFile(fileData, JSON.stringify(dogs, null, 4), (err) => {
-    if (err) {
-      console.log(`Error writing file: ${err}`);
-    }
-  });
+  file.writeFileSync(fileData, JSON.stringify(dogs, null, 4));
   return dogs;
 };
 
 module.exports = {
   getDogs,
   saveDog,
+  readDogs,
 };
--- server/routes/dogs.js
+++ server/routes/dogs.js
@@ -1,23 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Dog = require('../models/Dog');
-const { getDogs, saveDog } = require('../db');
+const Dog = require("../models/Dog");
+const { getDogs, saveDog } = require("../db");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.json(
-    getDogs().map((dog) => {
+    getDogs().map(dog => {
       return (dog = { name: dog.name, birth: dog.birth, age: dog.getAge() });
     })
   );
 });
 
-router.get('/:name', (req, res) => {
-  res.json(getDogs().filter((dog) => dog.name === req.params.name));
+router.get("/:name", (req, res) => {
+  res.json(getDogs().filter(dog => dog.name === req.params.name));
 });
 
-router.post('/add', (req, res) => {
+router.post("/add", (req, res) => {
   const dogs = saveDog(new Dog(req.body.name, req.body.birth));
-  res.json(dogs);
+  res.json(
+    getDogs().map(dog => {
+      return (dog = { name: dog.name, birth: dog.birth, age: dog.getAge() });
+    })
+  );
 });
 
 module.exports = router;
