5b80cb04690042268873d373d442ffedb139d707
Adam Zięba <adam.maciej.zieba@gmail.com>
2022-05-22 15:55:11 +0200

    path corrected

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,14 @@
 const file = require("fs");
 const path = require("path");
-const fileData = path.resolve(__dirname, "data", "dogs.json");
+const dataFileName = path.resolve(__dirname, "data", "dogs.json");
 const Dog = require("../models/Dog");
 
 const dogs = [];
 
 function readDogs() {
   try {
-    JSON.parse(file.readFileSync(fileData, "utf8")).forEach(dog => {
+    console.log(__dirname);
+    JSON.parse(file.readFileSync(dataFileName, "utf8")).forEach(dog => {
       dogs.push(new Dog(dog.name, dog.birth));
     });
   } catch (err) {
@@ -17,23 +18,29 @@ function readDogs() {
 
 const getDogs = () => {
   // try {
-  //   return JSON.parse(file.readFileSync(fileData)).map(dog => {
+  //   return JSON.parse(file.readFileSync("./data/"+dataFileName)).map(dog => {
   //     return new Dog(dog.name, dog.birth);
   //   });
   // } catch (err) {
-  //   console.log(`Error reading file from disk: ${err} ${fileData}`);
+  //   console.log(`Error reading file from disk: ${err} ${dataFileName}`);
   // }
   return dogs;
 };
 
 const saveDog = dog => {
   dogs.unshift(dog);
-  file.writeFile(fileData, JSON.stringify(dogs, null, 4), err => {
+  file.writeFile(dataFileName, JSON.stringify(dogs, null, 4), err => {
     if (err) {
       console.log(`Error writing file: ${err}`);
     }
   });
 };
+
+// file.writeFile("./data/" + dataFileName, "{}", err => {
+//   if (err) {
+//     console.log(`Error writing file: ${err}`);
+//   }
+// });
 
 module.exports = {
   getDogs,
