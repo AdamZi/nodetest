4c98cab0e71e3f0ec5e04c4bf0832edefcf75ace
Mateusz Rorat <mateuszrorat@gmail.com>
2022-05-23 08:54:42 +0200

    unittest added

--- server/models/Dog.test.js
+++ server/models/Dog.test.js
@@ -3,12 +3,19 @@ const Dog = require('./Dog');
 describe('Dog', () => {
   describe('toJSON', () => {
     const dog = new Dog('test', '2021');
-    test('should return specyfic structure of json', () => {
+    test('should return specific structure of json', () => {
       expect(dog.toJSON()).toEqual({
         age: 1,
         birth: '2021',
         name: 'test',
       });
+    });
+  });
+
+  describe('getNameToUpperCase', () => {
+    const dog = new Dog('test', '2021');
+    test('should return name to uppercase', () => {
+      expect(dog.getNameToUpperCase()).toEqual('NAME');
     });
   });
 });
